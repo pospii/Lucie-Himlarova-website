01744b0ba28ca5188879d5caa81a1357d330dbca
Adam Pospíšek <adam.pospisek@email.cz>
2024-10-21 16:29:59 +0200

    startslide

---
+++ js/startslide.js
@@ -0,0 +1,21 @@
+
+window.onload = function(){
+ const images = [
+    "img/uvod_kopie_upravena.jpeg",
+    "img/uvod_druha.jpeg",
+  ];
+
+  let currentIndex = 0;
+  const slider = document.getElementById('slider');
+
+  function changeBackground() {
+    currentIndex = (currentIndex + 1) % images.length; // Cyklické procházení obrázků
+    slider.style.backgroundImage = `url(${images[currentIndex]})`;
+  }
+
+  // První nastavení pozadí
+  slider.style.backgroundImage = `url(${images[currentIndex]})`;
+
+  // Nastavení intervalu pro změnu každých 5 sekund
+  setInterval(changeBackground, 7000);
+}
